14cb6c7012bf193ae1a4a2fe4f0cc097309465c3
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2020-12-21 21:27:32 +0545

    socket.io server added.

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const options = {
+  cors: {
+    origins: "*",
+  },
+};
+
+const io = require("socket.io")(server, options);
+
+io.on("connection", (socket) => {
+  socket.on("emit", (arg) => {
+    console.log(arg);
+    socket.broadcast.emit("broadcast", arg);
+  });
+});
+
+server.listen(3000);
