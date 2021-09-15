5f5bff8fafc861ccf3caa846ebfaba93144eba1a
kritish-dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-09-15 12:25:38 +0545

    :lipstick: main.cpp -> 01 Doubly Linked List.cpp

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const io = require("socket.io")(server, options);
 let state = {
   files: [
     {
-      name: "main.cpp",
+      name: "01 Doubly Linked List.cpp",
       content:
-        "#include<iostream>\nusing namespace std;\n\nint main(){\n  return 0;\n}",
+        '#include <iostream>\n\nstruct Node\n{\n int data;\n Node *prev, *next;\n};\n\nNode *head = NULL;\n\nvoid create(int A[], Node *head);\nvoid display(Node *head);\n\nint main()\n{\n int A[] = {2, 4, 6, 8, 10};\n create(A, head);\n std::cout << "HEAD" << std::endl;\n display(head);\n return 0;\n}\n\nvoid display(Node *p)\n{\n while (p)\n {\n std::cout << "[" << p->prev << "|" << p->data << "|" << p << "|" << p->next << "]" << std::endl;\n p = p->next;\n }\n}\n\nvoid create(int A[], Node *p)\n{\n Node *node = new Node;\n node->data = A[0];\n node->next = node->prev = NULL;\n\n head = node;\n\n Node *tail = head;\n\n for (int i = 1; i < 5; i++)\n {\n Node *node = new Node;\n node->data = A[i];\n node->next = NULL;\n node->prev = tail;\n\n tail->next = node;\n\n tail = node;\n }\n}',
     },
   ],
   active: 0,
